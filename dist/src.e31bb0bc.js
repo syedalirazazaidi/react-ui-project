3c511773d9331aefb36754c0a67d7a59b19c41da
ali raza <aaliraza007@hotmail.com>
2020-01-04 14:07:04 +0500

    React-Form

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -24007,7 +24007,7 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"components/square.js":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"../node_modules/react-dom/cjs/react-dom.development.js"}],"components/App.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -24020,97 +24020,6 @@ var _react = _interopRequireWildcard(require("react"));
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var style = {
-  backgroundColor: 'white',
-  border: 'black solid 2px',
-  textalign: 'center',
-  fontsize: '20px',
-  width: '40px',
-  height: '40px',
-  margin: -1,
-  display: 'inline-block',
-  verticalAlign: 'top' //  const tiles = ['', '0', 'X'];
-
-};
-
-var Square =
-/*#__PURE__*/
-function (_Component) {
-  _inherits(Square, _Component);
-
-  function Square() {
-    _classCallCheck(this, Square);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(Square).apply(this, arguments));
-  }
-
-  _createClass(Square, [{
-    key: "render",
-    // state ={
-    //     currentBoard  : 0,
-    // }
-    // handleClick=()=>{
-    //     this.setState(prevState=>{
-    //         return {
-    //             currentBoard  :(prevState.currentBoard + 4) % 3
-    //         }
-    //     })
-    // }
-    value: function render() {
-      return _react.default.createElement("div", {
-        style: style,
-        onClick: this.props.handleClick
-      }, [this.props.value]);
-    }
-  }]);
-
-  return Square;
-}(_react.Component);
-
-var _default = Square;
-exports.default = _default;
-},{"react":"../node_modules/react/index.js"}],"components/App.js":[function(require,module,exports) {
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-
-var _react = _interopRequireWildcard(require("react"));
-
-var _square = _interopRequireDefault(require("./square"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -24149,91 +24058,50 @@ function (_Component) {
     _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(App)).call.apply(_getPrototypeOf2, [this].concat(args)));
 
     _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "state", {
-      board: ["", "", "", "", "", "", "", "", ""],
-      turn: 'X'
-    });
-
-    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "playWon", function () {
-      var board = _this.state.board;
-      var winningCOnditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
-      for (var i = 0; i < winningCOnditions.length; i++) {
-        var _winningCOnditions$i = _slicedToArray(winningCOnditions[i], 3),
-            a = _winningCOnditions$i[0],
-            b = _winningCOnditions$i[1],
-            c = _winningCOnditions$i[2];
-
-        if (board[a] !== '' && board[a] === board[b] && board[b] === board[c]) {
-          return board[a];
-        }
-      }
-
-      var isBoardFull = true;
-
-      for (var _i2 = 0; _i2 < board.length; _i2++) {
-        if (board[_i2] === '') {
-          isBoardFull = false;
-          break;
-        }
-      }
-
-      if (!isBoardFull) {
-        return '';
-      } else {
-        return 'tie';
-      }
+      input: '',
+      valid: false
     });
 
     return _this;
   }
 
   _createClass(App, [{
-    key: "updatBoard",
-    value: function updatBoard(i) {
-      var board = this.state.board.slice();
-      var turn = this.state.turn;
-
-      if (board[i] == '') {
-        board[i] = turn;
-        turn = turn === '0' ? 'X' : '0';
-      }
-
-      this.setState({
-        board: board,
-        turn: turn
-      });
+    key: "handleInput",
+    value: function handleInput() {
+      console.log('you wrote ', this.state.input);
     }
   }, {
     key: "render",
     value: function render() {
-      return _react.default.createElement("div", null, _react.default.createElement("h1", null, this.playWon()), _react.default.createElement("div", null, "  ", _react.default.createElement(_square.default, {
-        value: this.state.board[0],
-        handleClick: this.updatBoard.bind(this, 0)
-      }), " ", _react.default.createElement(_square.default, {
-        value: this.state.board[1],
-        handleClick: this.updatBoard.bind(this, 1)
-      }), " ", _react.default.createElement(_square.default, {
-        value: this.state.board[2],
-        handleClick: this.updatBoard.bind(this, 2)
-      })), _react.default.createElement("div", null, "  ", _react.default.createElement(_square.default, {
-        value: this.state.board[3],
-        handleClick: this.updatBoard.bind(this, 3)
-      }), " ", _react.default.createElement(_square.default, {
-        value: this.state.board[4],
-        handleClick: this.updatBoard.bind(this, 4)
-      }), " ", _react.default.createElement(_square.default, {
-        value: this.state.board[5],
-        handleClick: this.updatBoard.bind(this, 5)
-      })), _react.default.createElement("div", null, "  ", _react.default.createElement(_square.default, {
-        value: this.state.board[6],
-        handleClick: this.updatBoard.bind(this, 6)
-      }), " ", _react.default.createElement(_square.default, {
-        value: this.state.board[7],
-        handleClick: this.updatBoard.bind(this, 7)
-      }), " ", _react.default.createElement(_square.default, {
-        value: this.state.board[8],
-        handleClick: this.updatBoard.bind(this, 8)
-      })));
+      var _this2 = this;
+
+      return _react.default.createElement("div", null, this.state.valid ? 'Valid Passowrd!' : "Invalid Password", _react.default.createElement("input", {
+        type: "text",
+        value: this.state.input,
+        onChange: function onChange(e) {
+          var value = e.target.value;
+          var valid;
+
+          if (value.length >= 8 && value.replace(/[^A-Z]/g, '').length >= 1) {
+            valid = true;
+          } else {
+            valid = false;
+          }
+
+          _this2.setState({
+            input: value,
+            valid: valid
+          }); //  this.setState({
+          //    input : e.target.value.toUpperCase()
+          //  })
+
+        },
+        ref: function ref(input) {
+          return _this2.inputField = input;
+        }
+      }), _react.default.createElement("button", {
+        onClick: this.handleInput.bind(this)
+      }, "Submit"));
     }
   }]);
 
@@ -24242,7 +24110,7 @@ function (_Component) {
 
 var _default = App;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js","./square":"components/square.js"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -24355,7 +24223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62268" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63244" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
